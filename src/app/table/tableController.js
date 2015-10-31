ce4cece4a9e6c9adede51ce3aaec61bb8b61ebc3
gkopevski <gkopevski@gmail.com>
2015-10-31 15:11:40 +0100

    fixed bug with displaying of the directives

--- src/app/table/tableController.js
+++ src/app/table/tableController.js
@@ -3,11 +3,11 @@
 
     angular.module('java2days.table')
         .controller('TableController',
-        function ($scope,moduleProvider) {
+        function ($scope, moduleProvider) {
 
-            $scope.starter = moduleProvider.get('starter').available;
-            $scope.meal = moduleProvider.get('meal').available;
-            $scope.dessert = moduleProvider.get('dessert').available;
+            $scope.starter = moduleProvider.get('starter') === undefined ? false : moduleProvider.get('starter').available;
+            $scope.meal = moduleProvider.get('meal') === undefined ? false : moduleProvider.get('meal').available;
+            $scope.dessert = moduleProvider.get('dessert') === undefined ? false : moduleProvider.get('dessert').available;
 
         });
 
