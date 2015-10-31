3edb3ccbe498d9209d5a54bce3b1e74b24df1c70
gkopevski <gkopevski@gmail.com>
2015-10-31 21:08:55 +0100

    removed some un needed dependencies

--- src/app/table/tableController.js
+++ src/app/table/tableController.js
@@ -4,11 +4,6 @@
     angular.module('java2days.table')
         .controller('TableController',
         function ($scope, moduleProvider) {
-
-            $scope.starter = moduleProvider.get('starter') === undefined ? false : moduleProvider.get('starter').available;
-            $scope.meal = moduleProvider.get('meal') === undefined ? false : moduleProvider.get('meal').available;
-            $scope.dessert = moduleProvider.get('dessert') === undefined ? false : moduleProvider.get('dessert').available;
-
         });
 
 }());
